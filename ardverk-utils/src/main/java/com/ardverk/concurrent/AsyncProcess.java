6a19482fe0e213fb457230fb903422cb09a998c9
Roger Kapsi <rkapsi@gmail.com>
2009-05-26 12:04:16 +0000

    Comment
    git-svn-id: file:///Users/roger/Desktop/ardverk/trunk@8 03c7a90b-ddfe-4006-9334-91d42bd31b87

--- ardverk-utils/src/main/java/com/ardverk/concurrent/AsyncProcess.java
+++ ardverk-utils/src/main/java/com/ardverk/concurrent/AsyncProcess.java
@@ -2,5 +2,9 @@ package com.ardverk.concurrent;
 
 public interface AsyncProcess<V> {
 
+    /**
+     * NOTE: This method is being called while a lock on 'future'
+     * is being held!
+     */
     public void start(AsyncFuture<V> future) throws Exception;
 }
