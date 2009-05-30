46e0a234c2d0605f4791f5410a59f0e9a4c2f7a7
Roger Kapsi <rkapsi@gmail.com>
2009-05-30 02:07:04 +0000

    Will need these methods at some point
    git-svn-id: file:///Users/roger/Desktop/ardverk/trunk@23 03c7a90b-ddfe-4006-9334-91d42bd31b87

--- ardverk-dht/src/main/java/com/ardverk/dht/message/MessageId.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/message/MessageId.java
@@ -1,11 +1,16 @@
 package com.ardverk.dht.message;
 
 import java.io.Serializable;
+import java.net.SocketAddress;
 
 import com.ardverk.io.Writable;
 
 public interface MessageId extends Writable, Serializable {
 
+    public boolean isTaggable();
+    
+    public boolean isFor(SocketAddress address);
+    
     public int length();
     
     public byte[] getBytes();
