1e896def9a7ccfd5ca03efdc91fda09cfde6789a
Roger Kapsi <rkapsi@gmail.com>
2009-07-15 22:56:42 +0000

    Comments
    git-svn-id: file:///Users/roger/Desktop/ardverk/trunk@76 03c7a90b-ddfe-4006-9334-91d42bd31b87

--- ardverk-utils/src/main/java/com/ardverk/collection/FixedSize.java
+++ ardverk-utils/src/main/java/com/ardverk/collection/FixedSize.java
@@ -1,8 +1,17 @@
 package com.ardverk.collection;
 
+/**
+ * An interface for ADTs that have a fixed size capacity.
+ */
 public interface FixedSize {
 
+    /**
+     * Returns the maximum size of the ADT.
+     */
     public int getMaxSize();
     
+    /**
+     * Returns true if the ADT is full.
+     */
     public boolean isFull();
 }
