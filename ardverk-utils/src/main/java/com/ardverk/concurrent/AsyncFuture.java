1ef87496d4e724373fa5ee0a0f279607c3a83ffb
Roger Kapsi <rkapsi@gmail.com>
2009-07-20 12:17:19 +0000

    Comments
    git-svn-id: file:///Users/roger/Desktop/ardverk/trunk@81 03c7a90b-ddfe-4006-9334-91d42bd31b87

--- ardverk-utils/src/main/java/com/ardverk/concurrent/AsyncFuture.java
+++ ardverk-utils/src/main/java/com/ardverk/concurrent/AsyncFuture.java
@@ -3,21 +3,59 @@ package com.ardverk.concurrent;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+/**
+ * 
+ */
 public interface AsyncFuture<V> extends Future<V> {
     
+    /**
+     * Sets the value of the {@link AsyncFuture}
+     */
     public void setValue(V value);
     
+    /**
+     * Sets the {@link Exception} of the {@link AsyncFuture}
+     */
     public void setException(Throwable t);
     
+    /**
+     * Returns true if the {@link AsyncFuture} finished due to a timeout.
+     */
     public boolean isTimeout();
     
+    /**
+     * A non-blocking version of the {@link #get()} method. It returns
+     * either null if the {@link AsyncFuture} is not done yet or the
+     * value and throws an Exception respectively.
+     */
     public V tryGet() throws InterruptedException, ExecutionException;
     
+    /**
+     * Returns true if the {@link AsyncFuture} is done and any of 
+     * the {@link #get()} methods will throw an {@link Exception}.
+     */
     public boolean throwsException();
     
+    /**
+     * Adds the given {@link AsyncFutureListener}.
+     * 
+     * NOTE: It's possible to add an {@link AsyncFutureListener} even after the 
+     * {@link AsyncFuture} is done and the listener will be notified through
+     * the Event Thread. The listener is however not being added to the 
+     * {@link AsyncFuture}'s listener list. That means it will not appear in 
+     * the list that is being returned by {@link #getAsyncFutureListeners()}.
+     */
     public void addAsyncFutureListener(AsyncFutureListener<V> l);
     
+    /**
+     * Removes the given {@link AsyncFutureListener}
+     */
     public void removeAsyncFutureListener(AsyncFutureListener<V> l);
     
+    /**
+     * Returns all {@link AsyncFutureListener}s
+     * 
+     * @see Please see {@link #addAsyncFutureListener(AsyncFutureListener)}
+     */
     public AsyncFutureListener<V>[] getAsyncFutureListeners();
 }
