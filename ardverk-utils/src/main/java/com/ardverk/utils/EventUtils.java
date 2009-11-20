882843118dcda1f982bdb0395a88a3b2bd0b3b8d
Roger Kapsi <rkapsi@gmail.com>
2009-11-20 14:14:07 +0000

    Use AsyncExecutors' Event Thread
    git-svn-id: file:///Users/roger/Desktop/ardverk/trunk@112 03c7a90b-ddfe-4006-9334-91d42bd31b87

--- ardverk-utils/src/main/java/com/ardverk/utils/EventUtils.java
+++ ardverk-utils/src/main/java/com/ardverk/utils/EventUtils.java
@@ -1,36 +1,17 @@
 package com.ardverk.utils;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicReference;
+import org.ardverk.concurrent.AsyncExecutors;
 
 public class EventUtils {
-
-    private static AtomicReference<Thread> REFERENCE 
-        = new AtomicReference<Thread>();
     
-    private static final ThreadFactory FACTORY
-            = new SimpleThreadFactory("EventUtilsThread") {
-        @Override
-        public Thread newThread(Runnable r) {
-            Thread thread = super.newThread(r);
-            REFERENCE.set(thread);
-            return thread;
-        }
-    };
-    
-    private static final Executor EXECUTOR 
-        = Executors.newSingleThreadExecutor(FACTORY);
-
     private EventUtils() {
     }
 
     public static boolean isEventThread() {
-        return REFERENCE.get() == Thread.currentThread();
+        return AsyncExecutors.isEventThread();
     }
     
     public static void fireEvent(Runnable event) {
-        EXECUTOR.execute(event);
+        AsyncExecutors.fireEvent(event);
     }
 }
