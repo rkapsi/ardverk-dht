5cd595b6324b01da375ef105a94f64ef3f594fb3
Roger Kapsi <rkapsi@gmail.com>
2009-11-22 22:08:40 +0000

    Hmm, need to think...
    git-svn-id: file:///Users/roger/Desktop/ardverk/trunk@127 03c7a90b-ddfe-4006-9334-91d42bd31b87

--- ardverk-dht/src/main/java/com/ardverk/dht/ArdverkDHT.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/ArdverkDHT.java
@@ -1,9 +1,16 @@
 package com.ardverk.dht;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.SocketAddress;
+
 import com.ardverk.dht.io.transport.Transport;
+import com.ardverk.dht.io.transport.TransportListener;
 
-public class ArdverkDHT {
+public class ArdverkDHT implements Closeable {
 
+    private final TransportListener listener = new Foo();
+    
     private final NodeManager nodeManager 
         = new NodeManager();
     
@@ -19,11 +26,25 @@ public class ArdverkDHT {
         }
         
         this.transport = transport;
-        
         nodeManager.add(nodeId);
+        
+        transport.addTransportListener(listener);
+    }
+    
+    @Override
+    public void close() {
+        transport.removeTransportListener(listener);
     }
     
     public Transport getTransport() {
         return transport;
+    }
+    
+    private class Foo implements TransportListener {
+
+        @Override
+        public void received(SocketAddress src, Object message)
+                throws IOException {
+        }
     }
 }
--- ardverk-dht/src/main/java/com/ardverk/dht/Node.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/Node.java
@@ -1,12 +1,25 @@
 package com.ardverk.dht;
 
 import java.io.Closeable;
+import java.net.SocketAddress;
+
+import com.ardverk.dht.message.Message;
+import com.ardverk.dht.routing.DefaultRouteTable;
+import com.ardverk.dht.routing.RouteTable;
 
 class Node implements Closeable {
 
+    private final RouteTable routeTable 
+        = new DefaultRouteTable(pinger, contactFactory, 
+                k, contactId, instanceId, address);
+    
     private final KUID nodeId;
     
     public Node(KUID nodeId) {
+        if (nodeId == null) {
+            throw new NullPointerException("nodeId");
+        }
+        
         this.nodeId = nodeId;
     }
 
@@ -17,5 +30,9 @@ class Node implements Closeable {
     
     public KUID getNodeId() {
         return nodeId;
+    }
+    
+    public void handleMessage(SocketAddress src, Message message) {
+        
     }
 }
