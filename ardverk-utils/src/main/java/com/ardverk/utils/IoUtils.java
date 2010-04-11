8cf4ed43f8a08a0f2e9b4dd7b7f0799026d889f0
Roger Kapsi <rkapsi@gmail.com>
2010-04-11 15:59:25 -0400

    Adding ability to flush()

--- ardverk-utils/src/main/java/com/ardverk/utils/IoUtils.java
+++ ardverk-utils/src/main/java/com/ardverk/utils/IoUtils.java
@@ -1,6 +1,7 @@
 package com.ardverk.utils;
 
 import java.io.Closeable;
+import java.io.Flushable;
 import java.io.IOException;
 import java.util.Collection;
 
@@ -42,6 +43,38 @@ public class IoUtils {
         if (closeables != null) {
             for (Closeable c : closeables) {
                 success |= close(c);
+            }
+        }
+        return success;
+    }
+    
+    public static boolean flush(Flushable flushable) {
+        if (flushable != null) {
+            try {
+                flushable.flush();
+                return true;
+            } catch (IOException err) {
+                LOG.error("IOException", err);
+            }
+        }
+        return false;
+    }
+    
+    public static boolean flushAll(Flushable... flushables) {
+        boolean success = false;
+        if (flushables != null) {
+            for (Flushable c : flushables) {
+                success |= flush(c);
+            }
+        }
+        return success;
+    }
+    
+    public static boolean flushAll(Collection<? extends Flushable> flushables) {
+        boolean success = false;
+        if (flushables != null) {
+            for (Flushable c : flushables) {
+                success |= flush(c);
             }
         }
         return success;
