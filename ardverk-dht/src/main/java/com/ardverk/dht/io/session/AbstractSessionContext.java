a4787e6059004035d01b4e9b797205f74119feca
Roger Kapsi <rkapsi@gmail.com>
2010-05-02 17:41:41 -0400

    Removing these two as they're not being used

--- ardverk-dht/src/main/java/com/ardverk/dht/io/session/AbstractSessionContext.java
+++
@@ -1,9 +0,0 @@
-package com.ardverk.dht.io.session;
-
-public abstract class AbstractSessionContext implements SessionContext {
-
-    @Override
-    public String toString() {
-        return "remote=" + getRemoteAddress().toString();
-    }
-}
