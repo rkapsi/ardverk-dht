546a5106a4c6260bed824255136d72b0923c8dc9
Roger Kapsi <rkapsi@gmail.com>
2010-05-05 08:47:05 -0400

    Adding a few features

--- ardverk-dht/src/main/java/com/ardverk/dht/net/CurrentAddress.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/net/CurrentAddress.java
@@ -1,6 +1,8 @@
 package com.ardverk.dht.net;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 
 import org.ardverk.collection.FixedSizeHashSet;
@@ -14,13 +16,24 @@ public class CurrentAddress {
 
     private final NetworkMask mask;
     
+    // We use a ByteBuffer because it implements equals() and hashCode()
     private final FixedSizeHashSet<ByteBuffer> history;
     
     private InetAddress current = null;
     
     private InetAddress temporary = null;
     
-    public CurrentAddress(NetworkMask mask, int count) {        
+    /**
+     * 
+     */
+    public CurrentAddress(NetworkMask mask, int count) {
+        this (null, mask, count);
+    }
+    
+    /**
+     * 
+     */
+    public CurrentAddress(InetAddress address, NetworkMask mask, int count) {
         if (mask == null) {
             throw new NullArgumentException("mask");
         }
@@ -29,8 +42,17 @@ public class CurrentAddress {
             throw new IllegalArgumentException("count=" + count);
         }
         
+        this.current = address;
         this.mask = mask;
         this.history = new FixedSizeHashSet<ByteBuffer>(count);
+    }
+    
+    /**
+     * 
+     */
+    public synchronized boolean set(SocketAddress src, SocketAddress address) {
+        return set(((InetSocketAddress)src).getAddress(), 
+                ((InetSocketAddress)address).getAddress());
     }
     
     /**
@@ -62,7 +84,6 @@ public class CurrentAddress {
             return false;
         }
         
-        // We use a ByteBuffer because it implements equals() and hashCode()
         ByteBuffer network = ByteBuffer.wrap(mask.mask(src));
         
         // Make sure we're not accepting proposals more than once from the 
@@ -82,7 +103,7 @@ public class CurrentAddress {
     }
     
     /**
-     * 
+     * Returns the current {@link InetAddress}
      */
     public synchronized InetAddress get() {
         return current;
