597e8d5da9d85969beda16daa357aef8509cdac9
Roger Kapsi <rkapsi@gmail.com>
2010-10-20 08:31:17 -0400

    More work on the Interface

--- ardverk-dht/src/main/java/com/ardverk/dht/DHT2.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/DHT2.java
@@ -3,6 +3,8 @@ package com.ardverk.dht;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.concurrent.TimeUnit;
 
@@ -15,17 +17,35 @@ import com.ardverk.dht.routing.Contact;
 import com.ardverk.dht.routing.RouteTable;
 
 public interface DHT2 {
-
-    public RouteTable getRouteTable();
     
-    public ArdverkFuture<PingEntity> ping(Contact dst, PingConfig config);
+    public ArdverkFuture<PingEntity> ping(String host, int port, PingConfig config);
+    
+    public ArdverkFuture<PingEntity> ping(InetAddress address, int port, PingConfig config);
+    
+    public ArdverkFuture<PingEntity> ping(SocketAddress address, PingConfig config);
     
     public ArdverkFuture<NodeEntity> lookup(KUID key, LookupConfig config);
+    
+    public ArdverkFuture<ValueEntity> get(KUID key, ValueConfig config);
     
     public ArdverkFuture<StoreEntity> put(Contact[] dst, KUID key, 
             Value value, StoreConfig config);
     
-    public ArdverkFuture<ValueEntity> get(KUID key, ValueConfig config);
+    
+    public static abstract class AbstractDHT implements DHT2 {
+
+        @Override
+        public ArdverkFuture<PingEntity> ping(String host, int port, 
+                PingConfig config) {
+            return ping(new InetSocketAddress(host, port), config);
+        }
+
+        @Override
+        public ArdverkFuture<PingEntity> ping(InetAddress address, int port,
+                PingConfig config) {
+            return ping(new InetSocketAddress(address, port), config);
+        }
+    }
     
     public static interface Value {
         
@@ -34,7 +54,7 @@ public interface DHT2 {
         public InputStream getContent() throws IOException;
     }
     
-    public static class DefaultValue implements Value {
+    public static class ByteArrayValue implements Value {
 
         private final byte[] value;
         
@@ -42,11 +62,11 @@ public interface DHT2 {
         
         private final int length;
         
-        public DefaultValue(byte[] value) {
+        public ByteArrayValue(byte[] value) {
             this(value, 0, value.length);
         }
         
-        public DefaultValue(byte[] value, int offset, int length) {
+        public ByteArrayValue(byte[] value, int offset, int length) {
             if (offset < 0 || length < 0 || value.length < (offset+length)) {
                 throw new IllegalArgumentException(
                         "offset=" + offset + ", length=" + length);
@@ -69,7 +89,7 @@ public interface DHT2 {
         }
     }
     
-    public static interface ArdverkConfig {
+    public static interface Config {
         
         public void setTimeout(long timeout, TimeUnit unit);
         
@@ -78,9 +98,16 @@ public interface DHT2 {
         public long getTimeoutInMillis();
     }
     
-    public static class DefaultArdverkConfig implements ArdverkConfig {
+    public static class DefaultConfig implements Config {
         
         private volatile long timeoutInMillis;
+        
+        public DefaultConfig() {
+        }
+        
+        public DefaultConfig(long timeout, TimeUnit unit) {
+            setTimeout(timeout, unit);
+        }
         
         @Override
         public void setTimeout(long timeout, TimeUnit unit) {
@@ -131,42 +158,42 @@ public interface DHT2 {
         }
     }
     
-    public static interface PingConfig extends ArdverkConfig {
+    public static interface PingConfig extends Config {
         
     }
     
-    public static class DefaultPingConfig extends DefaultArdverkConfig 
+    public static class DefaultPingConfig extends DefaultConfig 
             implements PingConfig {
         
     }
     
-    public static interface LookupConfig extends ArdverkConfig {
+    public static interface LookupConfig extends Config {
         
         public KUID getLookupId();
         
         public Contact[] getContacts();
     }
     
-    public static class DefaultLookupConfig extends DefaultArdverkConfig 
+    public static class DefaultLookupConfig extends DefaultConfig 
             implements LookupConfig {
 
-        private final KUID contactId;
+        private final KUID lookupId;
         
         private final Contact[] contacts;
         
-        public DefaultLookupConfig(KUID contactId, Contact[] contacts) {
-            this.contactId = contactId;
+        public DefaultLookupConfig(KUID lookupId, Contact[] contacts) {
+            this.lookupId = lookupId;
             this.contacts = contacts;
         }
         
-        public DefaultLookupConfig(KUID contactId, RouteTable routeTable) {
-            this.contactId = contactId;
-            this.contacts = routeTable.select(contactId);
+        public DefaultLookupConfig(KUID lookupId, RouteTable routeTable) {
+            this.lookupId = lookupId;
+            this.contacts = routeTable.select(lookupId);
         }
         
         @Override
         public KUID getLookupId() {
-            return contactId;
+            return lookupId;
         }
 
         @Override
@@ -175,11 +202,28 @@ public interface DHT2 {
         }
     }
     
-    public static interface ValueConfig extends ArdverkConfig {
+    public static interface ValueConfig extends LookupConfig {
         
     }
     
-    public static interface StoreConfig extends ArdverkConfig {
+    public static class DefaultValueConfig extends DefaultLookupConfig 
+            implements ValueConfig {
+
+        public DefaultValueConfig(KUID lookupId, Contact[] contacts) {
+            super(lookupId, contacts);
+        }
+
+        public DefaultValueConfig(KUID lookupId, RouteTable routeTable) {
+            super(lookupId, routeTable);
+        }
+    }
+    
+    public static interface StoreConfig extends Config {
+        
+    }
+    
+    public static class DefaultStoreConfig extends DefaultConfig 
+            implements StoreConfig {
         
     }
 }
