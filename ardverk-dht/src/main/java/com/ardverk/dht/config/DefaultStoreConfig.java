eb7f61d2c72026bd83df6a5ac12517f6ab13a577
Roger Kapsi <rkapsi@gmail.com>
2010-11-01 09:37:23 -0400

    Removing some unused code

--- ardverk-dht/src/main/java/com/ardverk/dht/config/DefaultStoreConfig.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/config/DefaultStoreConfig.java
@@ -1,24 +1,7 @@
 package com.ardverk.dht.config;
 
-import java.util.concurrent.TimeUnit;
-
 public class DefaultStoreConfig extends DefaultConfig 
         implements StoreConfig {
-
-    private volatile LookupConfig lookupConfig = new DefaultLookupConfig();
     
-    @Override
-    public LookupConfig getLookupConfig() {
-        return lookupConfig;
-    }
     
-    @Override
-    public void setLookupConfig(LookupConfig lookupConfig) {
-        this.lookupConfig = lookupConfig;
-    }
-    
-    @Override
-    public long getCombinedTimeout(TimeUnit unit) {
-        return ConfigUtils.getTimeout(unit, this, lookupConfig);
-    }
 }
--- ardverk-dht/src/main/java/com/ardverk/dht/config/StoreConfig.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/config/StoreConfig.java
@@ -1,12 +1,5 @@
 package com.ardverk.dht.config;
 
-import java.util.concurrent.TimeUnit;
-
 public interface StoreConfig extends Config {
     
-    public LookupConfig getLookupConfig();
-    
-    public void setLookupConfig(LookupConfig lookupConfig);
-    
-    public long getCombinedTimeout(TimeUnit unit);
 }
