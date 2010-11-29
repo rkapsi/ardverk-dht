46a15400082eb34227e98232c5d0a7c84d212a16
Roger Kapsi <rkapsi@gmail.com>
2010-11-29 11:58:13 -0500

    JavaDoc

--- ardverk-dht/src/main/java/com/ardverk/dht/codec/MessageCodec.java
+++ ardverk-dht/src/main/java/com/ardverk/dht/codec/MessageCodec.java
@@ -6,22 +6,22 @@ import java.net.SocketAddress;
 import com.ardverk.dht.message.Message;
 
 /**
- * 
+ * A {@link MessageCodec} encodes and decodes {@link Message}s.
  */
 public interface MessageCodec {
     
     /**
-     * 
+     * Encodes the given {@link Message}.
      */
     public abstract byte[] encode(Message message) throws IOException;
     
     /**
-     * 
+     * Decodes the given bytes and returns it as a {@link Message}.
      */
     public Message decode(SocketAddress src, byte[] data) throws IOException;
     
     /**
-     * 
+     * Decodes the given bytes and returns it as a {@link Message}.
      */
     public abstract Message decode(SocketAddress src, 
             byte[] data, int offset, int length) throws IOException;
