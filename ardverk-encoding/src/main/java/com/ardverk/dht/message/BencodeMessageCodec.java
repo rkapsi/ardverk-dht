5528b797b220acc6f3523c8634df5a4fca6fcac3
Roger Kapsi <rkapsi@gmail.com>
2010-11-29 14:31:53 -0500

    Removing unused code

--- ardverk-encoding/src/main/java/com/ardverk/dht/message/BencodeMessageCodec.java
+++ ardverk-encoding/src/main/java/com/ardverk/dht/message/BencodeMessageCodec.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.SocketAddress;
 
+import org.ardverk.io.IoUtils;
 import org.ardverk.lang.NumberUtils;
 
 import com.ardverk.dht.codec.AbstractMessageCodec;
@@ -26,21 +27,28 @@ public class BencodeMessageCodec extends AbstractMessageCodec {
     @Override
     public Message decode(SocketAddress src, byte[] data, int offset, int length)
             throws IOException {
-        MessageInputStream in = new MessageInputStream(
-                new ByteArrayInputStream(data, offset, length));
         
-        return in.readMessage(src);
+        MessageInputStream in = null;
+        try {
+            in = new MessageInputStream(
+                    new ByteArrayInputStream(data, offset, length));
+            return in.readMessage(src);
+        } finally {
+            IoUtils.close(in);
+        }
     }
     
     @Override
-    public byte[] encode(Message message)
-            throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream(messageSize);
-        MessageOutputStream out = new MessageOutputStream(baos);
-        
-        out.writeMessage(message);
-        out.close();
-        
-        return baos.toByteArray();
+    public byte[] encode(Message message) throws IOException {
+        MessageOutputStream out = null;
+        try {
+            ByteArrayOutputStream baos 
+                = new ByteArrayOutputStream(messageSize);
+            out = new MessageOutputStream(baos);
+            out.writeMessage(message);
+            return baos.toByteArray();
+        } finally {
+            IoUtils.close(out);
+        }
     }
 }
