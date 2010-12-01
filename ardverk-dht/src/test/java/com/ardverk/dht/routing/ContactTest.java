4798b2af23e15e0c3633ca5162a558c4e3e3e602
Roger Kapsi <rkapsi@gmail.com>
2010-12-01 13:01:06 -0500

    Changing a few params

--- ardverk-dht/src/test/java/com/ardverk/dht/routing/ContactTest.java
+++ ardverk-dht/src/test/java/com/ardverk/dht/routing/ContactTest.java
@@ -22,13 +22,13 @@ public class ContactTest {
                 KUID.createRandom(20), 
                 0, new InetSocketAddress("www.apple.com", 2000));
         
-        Contact contact2 = new Contact(Type.SOLICITED, 
+        Contact contact2 = new Contact(Type.UNKNOWN, 
                 contact1.getId(), 
-                0, new InetSocketAddress("www.microsoft.com", 3000));
+                1, new InetSocketAddress("www.microsoft.com", 3000));
         
-        Contact contact3 = new Contact(Type.SOLICITED, 
+        Contact contact3 = new Contact(Type.UNSOLICITED, 
                 KUID.createRandom(20), 
-                0, new InetSocketAddress("www.google.com", 4000));
+                2, new InetSocketAddress("www.google.com", 4000));
         
         TestCase.assertTrue(contact1.equals(contact2));
         TestCase.assertTrue(contact2.equals(contact1));
