85a9786ff7066571d48908dd496f4d1f45e9b4d0
Roger Kapsi <rkapsi@gmail.com>
2011-04-28 09:11:22 -0400

    Testing BLOBs

--- components/core/src/main/java/org/ardverk/dht/message/ExternalContent.java
+++ components/core/src/main/java/org/ardverk/dht/message/ExternalContent.java
@@ -17,12 +17,11 @@
 package org.ardverk.dht.message;
 
 import java.io.Closeable;
-import java.io.FilterInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.ardverk.dht.concurrent.DHTFuture;
 import org.ardverk.dht.concurrent.DHTValueFuture;
+import org.ardverk.io.CloseAwareInputStream;
 import org.ardverk.io.IoUtils;
 
 public class ExternalContent extends AbstractContent implements Closeable {
@@ -42,7 +41,12 @@ public class ExternalContent extends AbstractContent implements Closeable {
     
     public ExternalContent(long contentLength, InputStream in) {
         if (in != null) {
-            in = new ContentInputStream(in);
+            in = new CloseAwareInputStream(in) {
+                @Override
+                protected void complete() {
+                    ExternalContent.this.complete();
+                }
+            };
         }
         
         this.contentLength = contentLength;
@@ -85,48 +89,5 @@ public class ExternalContent extends AbstractContent implements Closeable {
 
     private void complete() {
         future.setValue(null);
-    }
-    
-    private class ContentInputStream extends FilterInputStream {
-
-        public ContentInputStream(InputStream in) {
-            super(in);
-        }
-
-        @Override
-        public int read() throws IOException {
-            int value = super.read();
-            if (value < 0) {
-                complete();
-            }
-            return value;
-        }
-        
-        @Override
-        public int read(byte[] b) throws IOException {
-            int value = super.read(b);
-            if (value < 0) {
-                complete();
-            }
-            return value;
-        }
-        
-        @Override
-        public int read(byte[] b, int off, int len) throws IOException {
-            int value = super.read(b, off, len);
-            if (value < 0) {
-                complete();
-            }
-            return value;
-        }
-
-        @Override
-        public void close() throws IOException {
-            try {
-                super.close();
-            } finally {
-                complete();
-            }
-        }
     }
 }
