3774ea3a160ef16669d1990b4ed20e5562f85d1c
Roger Kapsi <rkapsi@gmail.com>
2011-05-20 09:20:47 -0400

    Use LinkedHashMap

--- components/store/src/main/java/org/ardverk/dht/storage/VectorClockMap.java
+++ components/store/src/main/java/org/ardverk/dht/storage/VectorClockMap.java
@@ -1,8 +1,8 @@
 package org.ardverk.dht.storage;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.ardverk.collection.CollectionUtils;
 import org.ardverk.dht.rsrc.Value;
@@ -11,84 +11,43 @@ import org.ardverk.version.VectorClock;
 
 public class VectorClockMap<K, V extends Value> {
     
-    private final List<Entry<K, V>> entries = new ArrayList<Entry<K, V>>();
+    private final Map<VectorClock<K>, V> map 
+        = new LinkedHashMap<VectorClock<K>, V>();
     
-    public void upsert(VectorClock<K> clock, V value) {
-        Entry<K, V> entry = new Entry<K, V>(clock, value);
+    public void upsert(VectorClock<K> key, V value) {
         
-        Iterator<Entry<K, V>> it = entries.iterator();
+        Iterator<VectorClock<K>> it = map.keySet().iterator();
         while (it.hasNext()) {
-            Entry<K, V> existing = it.next();
+            VectorClock<K> existing = it.next();
             
-            Occured occured = existing.compareTo(clock);
-            if (occured == Occured.AFTER) {
-                it.remove();
+            Occured occured = VectorClockUtils.compare(existing, key);
+            switch (occured) {
+                case AFTER:
+                    it.remove();
+                    break;
             }
-            
-            // TODO: What about BEFORE and IDENTICIAL?
         }
         
-        entries.add(entry);
+        map.put(key, value);
     }
     
-    public boolean remove(VectorClock<K> clock) {
-        if (clock == null && entries.size() == 1) {
-            entries.clear();
-            return true;
-        }
-        
-        boolean success = false;
-        Iterator<Entry<K, V>> it = entries.iterator();
-        while (it.hasNext()) {
-            Entry<K, V> existing = it.next();
-            
-            Occured occured = existing.compareTo(clock);
-            if (occured == Occured.IDENTICAL) {
-                it.remove();
-                success = true;
-            }
-        }
-        return success;
+    public boolean remove(VectorClock<K> key) {
+        return map.remove(key) != null;
     }
     
     public V value() {
-        if (!entries.isEmpty()) {
-            Entry<?, V> entry = CollectionUtils.last(entries);
-            return entry.value;
-        }
-        return null;
+        return CollectionUtils.last(map.values());
     }
     
     public Value[] values() {
-        Value[] values = new Value[entries.size()];
-        int index = 0;
-        for (Entry<?, V> entry : entries) {
-            values[index++] = entry.value;
-        }
-        return values;
+        return map.values().toArray(new Value[0]);
     }
     
     public int size() {
-        return entries.size();
+        return map.size();
     }
     
     public boolean isEmpty() {
-        return size() == 0;
-    }
-    
-    private static class Entry<K, V extends Value> {
-        
-        private final VectorClock<K> clock;
-        
-        private final V value;
-        
-        public Entry(VectorClock<K> clock, V value) {
-            this.clock = clock;
-            this.value = value;
-        }
-        
-        public Occured compareTo(VectorClock<K> other) {
-            return VectorClockUtils.compare(clock, other);
-        }
+        return map.isEmpty();
     }
 }
