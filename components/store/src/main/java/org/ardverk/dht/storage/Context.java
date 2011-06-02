d50ffb966e7d14c106c2bbf76d2af6c53285c46e
Roger Kapsi <rkapsi@gmail.com>
2011-06-02 17:47:52 -0400

    HttpClient's HeaderGroup has a JSR-133 issue. It's running into NPEs on multicore CPUs

--- components/store/src/main/java/org/ardverk/dht/storage/Context.java
+++ components/store/src/main/java/org/ardverk/dht/storage/Context.java
@@ -3,11 +3,13 @@ package org.ardverk.dht.storage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.http.Header;
 import org.apache.http.message.BasicHeader;
-import org.apache.http.message.HeaderGroup;
 import org.ardverk.io.DataUtils;
 import org.ardverk.io.Writable;
 import org.ardverk.utils.StringUtils;
@@ -32,7 +34,7 @@ public final class Context implements Properties, Writable, Cloneable {
     
     @Override
     public boolean containsHeader(String name) {
-        return group.containsHeader(name);
+        return group.contains(name);
     }
 
     @Override
@@ -42,27 +44,27 @@ public final class Context implements Properties, Writable, Cloneable {
 
     @Override
     public void addHeader(Header header) {
-        group.addHeader(header);
+        group.add(header);
     }
 
     @Override
     public Header[] getHeaders() {
-        return group.getAllHeaders();
+        return group.headers();
     }
 
     @Override
     public Header getFirstHeader(String name) {
-        return group.getFirstHeader(name);
+        return group.first(name);
     }
 
     @Override
     public Header[] getHeaders(String name) {
-        return group.getHeaders(name);
+        return group.headers(name);
     }
 
     @Override
     public Header getLastHeader(String name) {
-        return group.getLastHeader(name);
+        return group.last(name);
     }
 
     @Override
@@ -72,12 +74,13 @@ public final class Context implements Properties, Writable, Cloneable {
     
     @Override
     public void setHeader(Header header) {
-        group.updateHeader(header);
+        group.replace(header);
     }
 
     @Override
     public void setHeaders(Header... h) {
-        group.setHeaders(h);
+        group.clear();
+        group.addAll(h);
     }
 
     @Override
@@ -89,24 +92,22 @@ public final class Context implements Properties, Writable, Cloneable {
 
     @Override
     public void removeHeader(Header header) {
-        group.removeHeader(header);
+        group.remove(header);
     }
     
     @Override
     public void removeHeaders(Header... headers) {
         for (Header header : headers) {
-            group.removeHeader(header);
+            group.remove(header);
         }
     }
     
-    @SuppressWarnings("unchecked")
     @Override
     public Iterator<Header> iterator() {
         return group.iterator();
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public Iterator<Header> iterator(String name) {
         return group.iterator(name);
     }
@@ -156,5 +157,121 @@ public final class Context implements Properties, Writable, Cloneable {
         String name = StringUtils.readString(in);
         String value = StringUtils.readString(in);
         return new BasicHeader(name, value);
+    }
+    
+    /**
+     * NOTE: {@link org.apache.http.message.HeaderGroup} has a JSR-133
+     * related concurrency (initialization) issue. This class replicates 
+     * its functionality, avoids the problem by declaring the list as 
+     * {@code final} and speeds up some operations.
+     */
+    private static class HeaderGroup implements Iterable<Header> {
+        
+        private final List<Header> list = new ArrayList<Header>();
+        
+        @Override
+        public Iterator<Header> iterator() {
+            return list.iterator();
+        }
+        
+        public Iterator<Header> iterator(String name) {
+            return Arrays.asList(headers(name)).iterator();
+        }
+        
+        public boolean contains(String name) {
+            return first(name) != null;
+        }
+        
+        public void add(Header header) {
+            if (header != null) {
+                list.add(header);
+            }
+        }
+        
+        public void remove(Header header) {
+            if (header != null) {
+                list.remove(header);
+            }
+        }
+        
+        public void addAll(Header... headers) {
+            if (headers != null) {
+                for (Header header : headers) {
+                    add(header);
+                }
+            }
+        }
+        
+        public Header first(String name) {
+            name = name.toLowerCase();
+            
+            for (Header header : list) {
+                if (equalsIgnoreCase(name, header)) {
+                    return header;
+                }
+            }
+            return null;
+        }
+        
+        public Header last(String name) {
+            name = name.toLowerCase();
+            
+            for (int i = list.size()-1; i >= 0; --i) {
+                Header header = list.get(i);
+                if (equalsIgnoreCase(name, header)) {
+                    return header;
+                }
+            }
+            return null;
+        }
+        
+        public Header[] headers() {
+            return list.toArray(new Header[0]);
+        }
+        
+        public Header[] headers(String name) {
+            List<Header> dst = new ArrayList<Header>();
+            
+            name = name.toLowerCase();
+            for (Header header : list) {
+                if (equalsIgnoreCase(name, header)) {
+                    dst.add(header);
+                }
+            }
+            
+            return dst.toArray(new Header[0]);
+        }
+        
+        public void replace(Header header) {
+            String name = header.getName().toLowerCase();
+            
+            int size = list.size();
+            for (int i = 0; i < size; i++) {
+                Header other = list.get(i);
+                if (equalsIgnoreCase(name, other)) {
+                    list.set(i, header);
+                    return;
+                }
+            }
+            
+            add(header);
+        }
+        
+        public void clear() {
+            list.clear();
+        }
+        
+        @Override
+        public String toString() {
+            return list.toString();
+        }
+        
+        /**
+         * NOTE: It's being assumed that the {@link String} argument
+         * is in lower case (see {@link String#toLowerCase()}).
+         */
+        private static boolean equalsIgnoreCase(String name, Header header) {
+            return name.equals(header.getName().toLowerCase());
+        }
     }
 }
