f6b7bb6f83d27ffbf48cd83cf4fb697e978362a6
Roger Kapsi <rkapsi@gmail.com>
2012-04-07 11:12:48 -0400

    Updating copyright years

--- components/core/src/main/java/org/ardverk/dht/routing/AbstractContact.java
+++ components/core/src/main/java/org/ardverk/dht/routing/AbstractContact.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 Roger Kapsi
+ * Copyright 2009-2012 Roger Kapsi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
